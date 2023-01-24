2cff1320ceed5f81fd287b1ec30ce1acdd070504
JeremyWallace1 <jeremy.wallace@telus.com>
2023-01-24 14:12:37 -0700

    fix visible public_count class_method

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -9,10 +9,10 @@ module Visible
 
   class_methods do
     def public_count
-      where(status: 'public').public_count
+      where(status: 'public').count
     end
   end
-  
+
   def archived?
     status == 'archived'
   end
